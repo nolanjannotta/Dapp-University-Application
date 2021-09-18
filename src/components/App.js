ee60376126bb813a9e7134cbbb194ebeb9a95f5f
nolanjannotta <anjannotta@verizon.net>
2021-09-17 22:08:13 -0700

    change to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => {
+  
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
@@ -45,7 +45,7 @@ class App extends Component {
         </div>
       </div>
     );
-  }
+  
 }
 
 export default App;
